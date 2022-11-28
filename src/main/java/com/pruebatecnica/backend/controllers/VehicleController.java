3b33ca933241a7c7e94c0b88d7bb86d1f7544c1e
Andres Osso <andres.osso@gaffel.com.co>
2022-11-28 15:35:07 -0500

    Fix: Ajuste de cors para conexión con frontend

--- src/main/java/com/pruebatecnica/backend/controllers/VehicleController.java
+++ src/main/java/com/pruebatecnica/backend/controllers/VehicleController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.pruebatecnica.backend.services.interfaces.IVehicleService;
 
 @RestController
 @RequestMapping("/api/vehicle")
+@CrossOrigin(value =  {"*"})
 public class VehicleController {
     @Autowired
     private IVehicleService service;
